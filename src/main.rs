cd1c4646cafc36e443f4be5399dbf1f035e10225
Jankatay <barjanay@proton.me>
2024-10-02 12:27:36 -0500

    initialized the project and set up basic command stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about=None)]
+struct Args {
+    #[arg(short = 'e', long = "encode")]
+    encode: bool,
+    #[arg(short = 'd', long = "decode")]
+    decode: bool,
+    #[arg(short = 'i', long = "info")]
+    info: bool,
+
+    message: String,
+}
+
+pub fn main() {
+    let args: Args = Args::parse();
+    if args.encode && args.decode {
+        eprintln!("You can't encode and decode at the same time");
+        std::process::exit(1);
+    }
+    println!("Message:\t{}", args.message);
 }
